fd6e8f23dc822d0ae4ac7bb5abe3848e2de2f271
Prathik Reddy <tvkp333555@gmail.com>
2021-10-02 23:10:25 +0530

    created home and about pgs

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Common from "../src/components/Common"
 
 const About = () => {
     return (
         <div>
-            <h1>About pg</h1>
+            <Common title = "Welcome to About Us " link = "/contact" btntext="Contact Us" imgSrc="https://png.pngtree.com/illustrations/20190322/ourmid/pngtree-business-office-flat-character-meeting-png-image_37339.jpg"/>
         </div>
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Common from "../src/components/Common"
 const Home = () => {
-    return (
-        <div>
-            <h1>home pg</h1>
+  return (
+    <div>
+            <Common title = "Grow your business with " link = "/about" imgSrc = "https://image.freepik.com/free-vector/group-people-working-together_52683-28615.jpg" btntext="About Us"/>
         </div>
-    )
+  )
 }
 
 export default Home
