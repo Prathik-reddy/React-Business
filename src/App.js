534243312c956cc70f73650d3544ce1fc783729f
Prathik Reddy <tvkp333555@gmail.com>
2021-10-02 23:08:27 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Redirect, Route, Switch } from 'react-router';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import './App.css';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
-import Contact from './Contact'
+import Contact from './Contact';
+import Navbar from './components/Navbar';
 const  App = ()=> {
   return (
     <>
+    <Navbar/>
     <Switch>
       <Route exact path = "/" component = {Home}/>
       <Route exact path = "/about" component = {About}/>
