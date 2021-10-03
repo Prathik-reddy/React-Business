1297dd48703b437540fe4c1e3801753828e390e2
Prathik Reddy <tvkp333555@gmail.com>
2021-10-03 19:04:22 +0530

    added footer page

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="bg-light text-center ">
+                <p className="mt-3">Copyright 2021 PrathikReddy. All Rights Reserved</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
